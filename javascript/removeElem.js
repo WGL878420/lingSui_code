7ea45947e599fdbd98debe5c19530713109a1476
han yu <1807003141@qq.com>
2018-01-16 02:10:46 +0800

    删除指定元素.

--- javascript/removeElem.js
+++ javascript/removeElem.js
@@ -3,7 +3,7 @@
     删除指定元素
 */
 
-let array = [1, 3, 5, 3, 7, 3, 9];
+let array = [9, 3, 5, 3, 7, 3, 9, 6, 3];
 let item = 3;
 
 function remove_1(array, item) {
@@ -19,3 +19,17 @@ function remove_1(array, item) {
 }
 
 console.log(remove_1(array, item));
+
+// 方法二
+function remove_2(array, item) {
+  let newArray = array.slice(0);
+  for (let i = 0; i < newArray.length; i++) {
+    if (newArray[i] == item) {
+      let index = newArray.indexOf(item);
+      newArray.splice(index, 1);
+    }
+  }
+  return newArray;
+}
+
+console.log(remove_2(array, item));
