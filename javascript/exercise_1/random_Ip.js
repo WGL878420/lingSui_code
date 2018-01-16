8c1a05e18749a0652e04dd97c273e1987d3af1bd
han yu <1807003141@qq.com>
2018-01-16 20:31:39 +0800

    随机 IP.

---
+++ javascript/exercise_1/random_Ip.js
@@ -0,0 +1,17 @@
+
+/**
+ * 写一个函数，生成一个随机 IP 地址，
+ * 一个合法的 IP 地址为 0.0.0.0 ~ 255.255.255.255
+ */
+
+function randomIP() {
+  let randIP = [];
+  for (let i = 0; i < 4; i++) {
+    let ipNum = Math.floor(Math.random() * 256);
+    randIP.push(ipNum);
+  }
+  return randIP;
+}
+
+let ip =  randomIP().join('.');
+console.log(ip);
