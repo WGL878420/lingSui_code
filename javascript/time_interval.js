bec33e62b8b81b4a437e1599ee2e8cf4f011419d
han yu <1807003141@qq.com>
2018-01-16 18:06:59 +0800

    时间间隔.

---
+++ javascript/time_interval.js
@@ -0,0 +1,14 @@
+
+/*
+  写一个函数，获取从当前时间 到 指定日期的间隔时间
+*/
+
+function timeInterval(initDate) {
+  let time = Date.now() - Date.parse(initDate);
+  return time;
+}
+
+let initDate = "2017-02-08 10:30:24";
+let interval = timeInterval(initDate);
+
+console.log(interval);
