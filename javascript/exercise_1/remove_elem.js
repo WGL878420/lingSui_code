b583421107281caf486122b46c929f9b0cd735ac
han yu <1807003141@qq.com>
2018-04-05 16:49:46 +0800

    删除数组中所有指定元素.

--- javascript/exercise_1/remove_elem.js
+++ javascript/exercise_1/remove_elem.js
@@ -1,10 +1,11 @@
 /**
- *  删除数组指定元素
+ *  删除数组中所有指定元素
  */
 
 let array = [9, 3, 5, 3, 7, 3, 9, 6, 3];
 let item = 3;
 
+// 方法一
 function remove_1(array, item) {
   let newArray = [];
   for (let i = 0; i < array.length; i++) {
@@ -32,3 +33,23 @@ function remove_2(array, item) {
 }
 
 console.log(remove_2(array, item));
+
+// 方法三 | splice()
+function remove_3(arr, item) {
+  var newArray = [...arr];
+  for (index in newArray) {
+    if (newArray[index] === item) {
+      newArray.splice(index, 1);
+    }
+  }
+  return newArray;
+}
+
+console.log(remove_3(array, 3));
+
+// 方法四 | filter()
+function remove_4(arr, item) {
+  return arr.filter(current => current !== item);
+}
+
+console.log(remove_4(array, 3));
